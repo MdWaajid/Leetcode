93d1820bebb30e6f377af48be02b1390ca61128a
MdWaajid <136142976+MdWaajid@users.noreply.github.com>
2026-09-07 18:23:11 +0530

    Time: 0 ms (100.00%) | Memory: 47 MB (24.62%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int oldColor = image[sr][sc];
+
+        if (oldColor == color)
+            return image;
+
+        fill(image, sr, sc, oldColor, color);
+
+        return image;
+    }
+
+    public void fill(int[][] image, int r, int c, int oldColor, int color) {
+        if (r < 0 || r >= image.length ||
+            c < 0 || c >= image[0].length ||
+            image[r][c] != oldColor)
+            return;
+
+        image[r][c] = color;
+
+        fill(image, r + 1, c, oldColor, color);
+        fill(image, r - 1, c, oldColor, color);
+        fill(image, r, c + 1, oldColor, color);
+        fill(image, r, c - 1, oldColor, color);
+    }
+}
